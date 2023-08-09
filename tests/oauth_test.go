48374886fae6042f67a0f0c3697c3c9ddfe7be58
donknap <donknap@gmail.com>
2023-08-09 20:12:49 +0800

    整理测试用例权限

--- tests/oauth_test.go
+++ tests/oauth_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestOauth(t *testing.T) {
-	client := w7.NewClient("", "")
+	client := w7.NewClient(APP_ID, APP_SECRET)
 	loginUrl, err := client.OauthService.GetLoginUrl("http://s.w7.cc")
 
 	if err != nil {
